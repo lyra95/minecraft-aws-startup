2739dc0658713955ece69b38528781b69c408f04
조태혁 <95hyouka@gmail.com>
2021-07-03 23:08:23 +0900

    security group

--- index.ts
+++ index.ts
@@ -2,8 +2,28 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+// Set this with your friends IPs
+const group = new aws.ec2.SecurityGroup("Minecraft group", {
+    // friens to server
+    ingress: [
+        { protocol: "tcp", fromPort: 25535, toPort:25535, cidrBlocks: ["114.199.141.138/32"] }, // 나
+        { protocol: "tcp", fromPort: 25535, toPort:25535, cidrBlocks: ["218.158.70.233/32"] },  // 관우
+        { protocol: "tcp", fromPort: 25535, toPort:25535, cidrBlocks: ["210.107.131.84/32"] },    // 재훈
+    ],
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+    // server to friends
+    egress: [
+        { protocol: "tcp", fromPort: 25535, toPort:25535, cidrBlocks: ["114.199.141.138/32"] }, // 나
+        { protocol: "tcp", fromPort: 25535, toPort:25535, cidrBlocks: ["218.158.70.233/32"] },  // 관우
+        { protocol: "tcp", fromPort: 25535, toPort:25535, cidrBlocks: ["210.107.131.84/32"] },    // 재훈
+    ]
+});
+
+const server = new aws.ec2.Instance("webserver-www", {
+    instanceType: size,
+    vpcSecurityGroupIds: [ group.id ], // reference the security group resource above
+    ami: ami.id,
+});
+
+export const publicIp = server.publicIp;
+export const publicHostName = server.publicDns;
